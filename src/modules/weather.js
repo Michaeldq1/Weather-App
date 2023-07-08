f001fa1d7159df2c4436412a8def953f0785af03
Michaeldq1 <michaeldquinton@gmail.com>
2023-07-08 12:26:21 -0500

    Weather.js code cleanup

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -148,17 +148,3 @@ async function getWeatherData(location, isFahrenheit) {
 }
 
 export { getWeatherData };
-
-//https://www.pexels.com/photo/lightnings-over-town-6312513/
-//https://www.pexels.com/photo/a-full-moon-in-the-night-sky-5489557/
-//https://www.pexels.com/photo/full-moon-in-the-night-sky-8681259/
-//https://www.pexels.com/photo/white-cumulus-clouds-and-blue-sky-8257786/
-//https://www.pexels.com/photo/white-clouds-in-the-sky-8753960/
-//https://www.pexels.com/photo/mist-on-clear-glass-on-a-rainy-weather-11963069/
-//https://www.pexels.com/photo/rural-snowy-village-during-severe-blizzard-4969828/
-//https://www.pexels.com/photo/trees-surrounded-by-water-during-foggy-day-3575854/
-//https://www.pexels.com/photo/photograph-of-a-wet-glass-surface-14381724/
-
-// 1. reorder location info section
-// 1. grab icons of additional info.
-// 3. organize additional info section.
