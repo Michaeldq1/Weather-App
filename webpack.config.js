936e32699a9564e180096363e4b58a678eef68f1
Michaeldq1 <michaeldquinton@gmail.com>
2023-07-08 13:43:10 -0500

    remove gh-pages branch

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "inline-source-map",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
